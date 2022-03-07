dbc2e0b0913f599036272b4a98cfc8a8c1048d66
ChenY <13308196164@163.com>
2022-03-07 09:43:20 +0800

    chore(util): 删除无用函数

--- utils/util.go
+++ utils/util.go
@@ -4,12 +4,6 @@ import (
 	"path/filepath"
 )
 
-func ReverseStrings(s []string) {
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
-	}
-}
-
 func FileNameStrip(fullName string) string {
 	fileNameBase := filepath.Base(fullName)
 	fileSuffix := filepath.Ext(fullName)
